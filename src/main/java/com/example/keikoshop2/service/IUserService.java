d5ad8ff0c5be9b4c3ded3318cda8ba2963875d50
NorthenCat <northencat@gmail.com>
2024-12-03 20:39:53 +0700

    fixing login service

--- src/main/java/com/example/keikoshop2/service/IUserService.java
+++ src/main/java/com/example/keikoshop2/service/IUserService.java
@@ -4,6 +4,6 @@ import com.example.keikoshop2.model.User;
 
 public interface IUserService{
     User register(User user);
-    void login(String username, String password);
+    User login(String username, String password);
     void logout(String username);
 }
