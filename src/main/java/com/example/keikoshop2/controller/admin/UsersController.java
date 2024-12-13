feb7c363eb28a849e67073607ae9628090827842
NorthenCat <northencat@gmail.com>
2024-12-13 19:46:55 +0700

    UPDATE : MERGE

--- src/main/java/com/example/keikoshop2/controller/admin/UsersController.java
+++ src/main/java/com/example/keikoshop2/controller/admin/UsersController.java
@@ -23,7 +23,7 @@ import java.util.List;
 @Controller
 @PreAuthorize("hasRole('admin')")
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/admin/users")
 public class UsersController {
 	private final IUserService userService;
 	private static final Logger logger = LoggerFactory.getLogger(UsersController.class);
@@ -43,7 +43,7 @@ public class UsersController {
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
 		}
-		return "redirect:/users/manage-users";
+		return "redirect:/admin/users/manage-users";
 	}
 
 	@PutMapping("/update/{id}")
@@ -55,7 +55,7 @@ public class UsersController {
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
 		}
-		return "redirect:/users/manage-users";
+		return "redirect:/admin/users/manage-users";
 	}
 
 	@DeleteMapping("/delete/{id}")
@@ -66,7 +66,7 @@ public class UsersController {
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
 		}
-		return "redirect:/users/manage-users";
+		return "redirect:/admin/users/manage-users";
 	}
 
 	@GetMapping("/getUsers/{id}")
