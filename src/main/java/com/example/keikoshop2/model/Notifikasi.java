b20def0ff41138bc1f734396d9760347d068b014
nabilakeiko <nabila.keiko22@gmail.com>
2025-01-01 21:05:49 +0700

    update:model Notifikasi|error : error minor gatau dimana cuman aman harusnya

---
+++ src/main/java/com/example/keikoshop2/model/Notifikasi.java
@@ -0,0 +1,70 @@
+package com.example.keikoshop2.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+public class Notifikasi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int userId;
+    private String title;
+    private String message;
+    private String status;
+    private boolean isRead;
+
+    public Notifikasi(int userId, String title, String message, String recipient, String status, boolean isRead) {
+        this.userId = userId;
+        this.title = title;
+        this.message = message;
+        this.status = status;
+        this.isRead = isRead;
+    }
+
+    public Notifikasi() {
+        this.isRead = false;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void serRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
